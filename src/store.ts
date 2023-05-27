a48f4b25f4aa3dff8c25736d16eae8fa0502e2d4
Derek Wan <d.wan@icloud.com>
2023-05-28 00:53:51 +0800

    Clean up demo repos (#15)

--- src/store.ts
+++
@@ -1,9 +0,0 @@
-import { cakeReducer } from "./features/cake/cakeSlice";
-import { icecreamReducer } from "./features/icecream/icecreamSlice";
-import { configureStore } from "@reduxjs/toolkit";
-
-const store = configureStore({
-  reducer: { cake: cakeReducer, icecream: icecreamReducer },
-});
-
-export { store };
